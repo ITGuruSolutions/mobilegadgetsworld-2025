55409a928f078ef07d137189db28ab5c1d402a15
ITGuruSolutions <itgurusolutions07@gmail.com>
2025-11-14 11:48:35 +0530

    fixed work

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,15 +21,26 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-    return (
-    <ClerkProvider>
-    <html lang="en">
+    const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+    return publishableKey ? (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${outfit.className} antialiased`}>
+                    <StoreProvider>
+                        <Toaster />
+                        {children}
+                    </StoreProvider>
+                </body>
+            </html>
+        </ClerkProvider>
+    ) : (
+        <html lang="en">
             <body className={`${outfit.className} antialiased`}>
                 <StoreProvider>
                     <Toaster />
                     {children}
                 </StoreProvider>
             </body>
-        </html> </ClerkProvider>
+        </html>
     );
 }
